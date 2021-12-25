c909f91aa987808cecae746750d692ec707a50c4
Hanada-Takao <taaa0823@gmail.com>
2021-12-25 15:44:44 +0900

    validate_add

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :varidate_name_not_including_comma
+
+  private
+
+  def varidate_name_not_including_comma
+    errors.add(:name, 'にコンマを含めることはできません') if name&.include?(',')
+  end
 end
