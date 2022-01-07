6e6cde981a62ec6d682fbf6e12f6ad13fb982937
Hanada-Takao <taaa0823@gmail.com>
2022-01-07 10:45:25 +0900

    AndSeach_PripritySort_Pagenation_mounting

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,18 @@
 class TasksController < ApplicationController
   def index
     if params[:sort_expired]
-      @tasks = Task.order('deadline ASC')
+      @tasks = Task.page(params[:page]).per(5).order('deadline ASC')
+    elsif params[:pri_sort]
+      @tasks = Task.page(params[:page]).per(5).order('priority ASC')
     else
-      @tasks = Task.order('created_at DESC')
+      @tasks = Task.page(params[:page]).per(5).order('created_at DESC')
     end
+  end
+
+  def search
+    @search_params = task_search_params
+    @tasks = Task.page(params[:page]).per(5).searchh(@search_params)
+      render :index
   end
 
   def show
@@ -44,6 +52,11 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :deadline, :status)
+    params.require(:task).permit(:name, :description, :deadline, :status, :priority)
+    .merge(status: params[:task][:status].to_i).merge(priority: params[:task][:priority].to_i)
+  end
+
+  def task_search_params
+    params.fetch(:search, {}).permit(:name, :status)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,10 @@ class Task < ApplicationRecord
   validate :varidate_name_not_including_comma
 
   scope :recent, -> { order(created_at: :desc)}
+  scope :priority_sorted, -> { order(priority: :desc) }
 
   enum status:{未着手:1, 着手:2, 完了:3}
+  enum priority:{高:1, 中:2, 低:3}
 
   private
 
@@ -17,4 +19,17 @@ class Task < ApplicationRecord
   def self.lastest(number)
     order(created_at: :desc).limit(number)
   end
+
+  scope :searchh, -> (search_params) do
+    return if search_params.blank?
+
+    name_like(search_params[:name]).status(search_params[:status])
+  end
+
+  scope :name_like, -> (name) { where('name LIKE ?', "%#{ name }%") if name.present? }
+  scope :status, -> (status) { where(status: status) if status.present? }
 end
+
+
+
+
