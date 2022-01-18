fcc24cd1e5113ce82f3e85e1bdbb270dcdddb63a
Hanada-Takao <taaa0823@gmail.com>
2022-01-18 19:37:39 +0900

    seeddata_create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 5.times do |n|
   User.create!(
-    name: "sample_name#{n+1}",
-    email: "sample_email#{n+1}@sample.com",
-    password: "sample_email#{n+1}@sample.com",
-    password_confirmation: "sample_email#{n+1}@sample.com",
+    name: "sample_name#{n + 1}",
+    email: "sample#{n + 1}@sample.com",
+    password: "sample#{n + 1}@sample.com",
+    password_confirmation: "sample#{n + 1}@sample.com",
     admin: "true"
   )
 end
 5.times do |n|
   User.create!(
-    name: "sample_name#{n+10}",
-    email: "sample_email#{n+10}@sample.com",
-    password: "sample_email#{n+10}@sample.com",
-    password_confirmation: "sample_email#{n+10}@sample.com",
+    name: "sample_name#{n + 10}",
+    email: "sample#{n+10}@sample.com",
+    password: "sample#{n+10}@sample.com",
+    password_confirmation: "sample#{n + 10}@sample.com",
     admin: "false"
   )
 end
@@ -23,8 +23,8 @@ end
 
 10.times do |n|
   Task.create!(
-    name: "sample_name#{i + 1}",
-    description: "sample_description#{n+1}",
+    name: "sample_name#{n + 1}",
+    description: "sample_description#{n + 1}",
     deadline: "2022-01-#{rand(1..31)}",
     status: "未着手",
     priority: "低",
@@ -32,9 +32,9 @@ end
   )
 end
 
-10.times do |n|
-  Task_tag.create!(
-    task_id: "#{Task.all.pluck(:id).sample}",
-    label_id: "#{Label.all.pluck(:id).sample}"
-  )
-end
+# 10.times do |n|
+#   Task_tag.create!(
+#     task_id: "#{Task.all.pluck(:id).sample}",
+#     tag_id: "#{Tag.all.pluck(:id).sample}"
+#   )
+# end
